36229c25328ba0fe405a18f0df41aea0e8650d6f
Ahmed Sobhi <humanzz@hotmail.com>
2008-09-06 06:18:27 +0200

    cleaning and drying up things

--- lib/version_cache.rb
+++ lib/version_cache.rb
@@ -7,11 +7,11 @@ module VersionCache
     end
     
     module ClassMethods
-      def has_cache_associates(*associates)
-        @cache_associates = associates
-      end
+      def has_cache_associates(*associates);@cache_associates = associates;end
       
       def cache_associates;@cache_associates||=[];end
+	  
+      def cache_version_key(id);"#{self.name}_#{id}";end
     end
     
     module InstanceMethods
@@ -26,20 +26,22 @@ module VersionCache
       end
       
       def cache_version
-        key = self.class.name + "_#{self.id}"
-        unless(version = Rails.cache.read(key))
+        unless(version = Rails.cache.read(cache_version_key))
           version = 0
         end
         version
       end
       
       def increment_cache_version
-        key = self.class.name + "_#{self.id}"
-        unless(version = Rails.cache.read(key))
+        unless(version = Rails.cache.read(cache_version_key))
           version = 0
         end
-        Rails.cache.write(key, version + 1)
+        Rails.cache.write(cache_version_key, version + 1)
       end
+	  
+	  def cache_version_key
+		@cache_version_key ||= self.class.cache_version_key(self.id)
+	  end
     end
   end
 end
@@ -52,9 +54,14 @@ module VersionCache
     
     module ClassMethods
       protected
-      # model class for which we'll do version caching
-      # associates model members that their versions need update as well
-      # options = {:expiry => number of minutes to keep in memcache maximum, :browser_cache_enabled}
+      
+      # Caches certain action (show unless specified) based on a certain object's (ActiveRecord) version
+      # It caches the page in the cache store and sets the etag header for the response
+      # It takes the following parameters
+      # model: the object's class 
+      # options: a hash which can accept any of the following
+      # :action for specifying an action other than :show
+      # :expiry to specify the maximum number of minutes to keep the page in the cache
       def version_cache(model, options = {})
         return unless ::ActionController::Base.perform_caching
         include InstanceMethods unless self.included_modules.include?(VersionCache::ActionController::InstanceMethods)
@@ -63,7 +70,7 @@ module VersionCache
         self.instance_variable_set("@version_cache_#{model.name}_#{action}_options".to_sym,options)
         class_eval <<-"end_eval"
         def version_cache_#{model.name}_#{action}
-          unless (version = Rails.cache.read("#{model.name}_" + version_cache_model_id))
+          unless (version = Rails.cache.read(#{model.name}.cache_version_key(version_cache_model_id)))
             version = 0
           end
           vckp = version_cache_key_part
@@ -77,7 +84,12 @@ module VersionCache
         around_filter :version_cache_#{model.name}_#{action}, :only => [:#{action}]
         end_eval
       end
-
+      
+      # Caches certain actions for a specified period of time
+      # actions is a hash of the form {:action_name => action_options_hash}
+      # example: {:index => {:expiry => 10}, {:show => {:expiry => 5}}}
+      # The action_options_hash options are
+      # :expiry to specify the maximum number of minutes to keep the page in the cache
       def time_cache(actions = {})
         return if(!::ActionController::Base.perform_caching || actions.keys.length == 0)
         include InstanceMethods unless self.included_modules.include?(VersionCache::ActionController::InstanceMethods)
@@ -118,43 +130,38 @@ module VersionCache
           return
         end
         
-        options = {:expiry => 0, :browser_cache_enabled => false}
+        options = {:expiry => 0, :expires_header => false}
         options = options.merge(keys.delete_at(keys.length - 1)) if keys.length != 0 && keys[keys.length - 1].is_a?(Hash)
         key = "#{request.host}:#{request.request_uri}:#{keys * ':'}"
-        etag = Digest::MD5.hexdigest(key)
-        logger.info ">>>>>cache_response options #{options.inspect}"
-        logger.info ">>>>>cache_response key #{key}"
+        etag = Digest::MD5.hexdigest(key)        
         
         # first handle HTTP, lets us avoid a memcache hit
         # and saves a huge amount of bandwidth to the client
         if request.env["HTTP_IF_NONE_MATCH"] == etag
-          headers["X-Cache"] = "HTTP"
+          response.headers["X-Cache"] = "HTTP"
           head :not_modified
+          logger.info "version_cache: 304 Not Modified"
           return
         end
         
         # Next check memcache
         if data = Rails.cache.read(key)
           # render from the cached values
-          headers["Content-Type"] = data[:content_type]
-          headers["X-Cache"] = "HIT"
+          response.headers["Content-Type"] = data[:content_type]
+          response.headers["X-Cache"] = "HIT"
           render :text=>data[:content], :status=>data[:status]
+		      logger.info "version_cache: cache hit"
         else
           # Finally, yield, indicate we've missed then cache the response
-          headers["X-Cache"] = "MISS"
+          response.headers["X-Cache"] = "MISS"
           yield
           #cache 200 OK reposnes only
           if headers["Status"].to_i == 200
-            if options[:browser_cache_enabled]
-              response.headers["Cache-Control"] = "public, max-age=#{options[:expiry].minutes}"
-              headers["Expires"] = options[:expiry].minutes.from_now.httpdate
-            else
-              response.headers["ETag"] = etag
-            end
-            
+            response.headers["ETag"] = etag
             expiry = options[:expiry] == 0 ? 0 : (options[:expiry].minutes.from_now - Time.now).to_i
             Rails.cache.write(key, {:content=>response.body, :status=> headers["Status"].to_i, :content_type=>(response.content_type || "text/html")}, :expires_in => expiry)
           end
+		      logger.info "version_cache: cache miss"
         end
       end 
       
